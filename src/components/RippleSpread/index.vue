a9a5335cc65e97913f4cd7ba4ab611dcdc5ad31a
ChenErik <2392237608@qq.com>
2022-05-18 09:58:43 +0800

    feat(components):[add RippleSpread]

---
+++ src/components/RippleSpread/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+interface Props {
+    num:number
+    color:string
+    strokeWidth:number
+    duration:number
+}
+withDefaults(defineProps<Props>(),{
+    num:3,
+    color:'#f5222d',
+    strokeWidth:5,
+    duration: 3
+})
+const formatDuration = (duration:number):string|number => {
+  return duration === 0 ? 0 : `${duration}s`
+}
+</script>
+
+<template>
+  <svg viewBox="0 0 100 100">
+    <circle v-for="(item, index) in num" :key="item" cx="50" cy="50" r="45" :stroke="color" :stroke-width="strokeWidth" fill-opacity="0">
+      <animate id="ani1" attributeName="r" :begin="(duration / num) * index" from="0" to="45" :dur="formatDuration(duration)" repeatCount="indefinite" />
+      <animate attributeName="opacity" :begin="(duration / num) * index" from="1" to="0" :dur="formatDuration(duration)" repeatCount="indefinite" />
+    </circle>
+  </svg>
+</template>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,7 +2,11 @@
     <div class="dashboard" style="resize: both;">
         {{ $route.meta.title }}
         <!-- <el-input v-model="input" placeholder="Please input" /> -->
+        <div style="width:500px;height:500px">
+            <RippleSpread :num="8" />
+        </div>
     </div>
 </template>
 <script lang="ts" setup name="Dashboard">
+import RippleSpread from '@/components/RippleSpread/index.vue'
 </script>
