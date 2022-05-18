bf16699bbf2bc7e5914aee5d1ef72505514f897f
ChenErik <2392237608@qq.com>
2022-05-18 10:22:24 +0800

    fix(bug):[component] RippleSpread radius default value

--- src/components/RippleSpread/index.vue
+++ src/components/RippleSpread/index.vue
@@ -18,7 +18,7 @@ const formatDuration = (duration:number):string|number => {
 
 <template>
   <svg viewBox="0 0 100 100">
-    <circle v-for="(item, index) in num" :key="item" cx="50" cy="50" r="45" :stroke="color" :stroke-width="strokeWidth" fill-opacity="0">
+    <circle v-for="(item, index) in num" :key="item" cx="50" cy="50" r="0" :stroke="color" :stroke-width="strokeWidth" fill-opacity="0">
       <animate id="ani1" attributeName="r" :begin="(duration / num) * index" from="0" to="45" :dur="formatDuration(duration)" repeatCount="indefinite" />
       <animate attributeName="opacity" :begin="(duration / num) * index" from="1" to="0" :dur="formatDuration(duration)" repeatCount="indefinite" />
     </circle>
